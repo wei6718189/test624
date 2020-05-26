85beb7b1c966ecd47939d0392a8c0f29bc9e8454
weiweihua <597609230@qq.com>
2020-05-27 00:59:33 +0800

    提交tow commit 1 2 31
    提交tow b2
    
    提交tow b3

--- app/src/main/java/com/example/myapplication624/tow.java
+++ app/src/main/java/com/example/myapplication624/tow.java
@@ -2,4 +2,7 @@ package com.example.myapplication624;
 
 public class tow {
     private static String A;
+    private static String B1;
+    private static String B2;
+    private static String B3;
 }
