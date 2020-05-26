cbf631ceed406a73b422c5c92b0a0c4f54f23e01
weiweihua <597609230@qq.com>
2020-05-26 21:42:31 +0800

    提交两个文件

--- app/src/main/java/com/example/myapplication624/V01.java
+++ app/src/main/java/com/example/myapplication624/V01.java
@@ -1,4 +1,6 @@
 package com.example.myapplication624;
 
 public class V01 {
+    //test1
+    private static final String TAG = "V01";
 }
---
+++ app/src/main/java/com/example/myapplication624/V011.java
@@ -0,0 +1,4 @@
+package com.example.myapplication624;
+
+public class V011 {
+}
